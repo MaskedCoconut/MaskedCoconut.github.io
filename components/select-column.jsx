d5cc6e745a1d407c8246368a2518cc989e22ec36
Antoine Glacet <glacet.antoine@gmail.com>
2023-04-22 16:53:22 +0900

    styling

--- components/select-column.jsx
+++ components/select-column.jsx
@@ -1,33 +1,49 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { Stack } from "@mui/material";
 
+export default function BasicSelect(props) {
+  const selectList = ["Airline", "Seats", "scheduled"];
+  const [age, setAge] = React.useState("");
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
 
-
-export default function BasicSelect() {
-    const [age, setAge] = React.useState('');
-    const choices = ['airline', 'scheduled date', 'seats', 'pax']
-    const handleChange = (event) => {
-        setAge(event.target.value);
-    };
-
-    return (
-        <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={age}
-                    label="Age"
-                    onChange={handleChange}
-                >
-                    {choices.map((val) => <MenuItem key={val} value={val}> {val} </MenuItem>)}
-                </Select>
-            </FormControl>
+  return (
+    <Stack
+      spacing={{ xs: 1, sm: 2 }}
+      direction="row"
+      useFlexGap
+      flexWrap="wrap"
+    >
+      {selectList.map((val) => (
+        <Box key={"box" + val} sx={{ minWidth: 120 }}>
+          <FormControl key={"form" + val} fullWidth>
+            <InputLabel key={"input label" + val} id="demo-simple-select-label">
+              {val}
+            </InputLabel>
+            <Select
+              key={"select" + val}
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label={val}
+              onChange={handleChange}
+            >
+              {props.choices.map((val) => (
+                <MenuItem key={val} value={val}>
+                  {" "}
+                  {val}{" "}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
         </Box>
-    );
+      ))}
+    </Stack>
+  );
 }
