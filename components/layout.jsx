3eb7e3edca9537c86becbe22d9501897621646c8
Antoine Glacet <glacet.antoine@gmail.com>
2023-05-03 01:54:07 +0900

    favicon shenanigans

--- components/layout.jsx
+++ components/layout.jsx
@@ -4,6 +4,7 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Footer from "./UI/footer";
 import AppProvider from "../components/context/AppDataContext";
 import Box from "@mui/material/Box";
+import Head from "next/head";
 
 const theme = createTheme({
   components: {
@@ -26,6 +27,29 @@ export default function Layout({ children }) {
   return (
     <AppProvider>
       <ThemeProvider theme={theme}>
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <AppBar />
         <Box minHeight={500}>{children}</Box>
         <Footer />
