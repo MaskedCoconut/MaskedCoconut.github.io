62bd612241f811474790889c11bbeba600e76238
Antoine Glacet <glacet.antoine@gmail.com>
2023-05-06 15:13:52 +0900

    fix issue

--- pages/index.jsx
+++ pages/index.jsx
@@ -60,11 +60,7 @@ function TabPanel(props) {
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
+      {value === index && <>{children}</>}
     </div>
   );
 }
